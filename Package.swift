2eb5450d0947ba8671ab23bd0590e110e08a9908
Martin Stamenkovski <stamenkovskimar@gmail.com>
2020-02-11 20:45:15 +0100

    watchOS version fix

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v10),
         .macOS(.v10_12),
-        .watchOS(.v3_0)
+        .watchOS(.v3)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
