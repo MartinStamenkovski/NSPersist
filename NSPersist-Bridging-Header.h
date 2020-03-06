31848691d616f1c0ee04a9ed9ece7894748cf1f2
Martin Stamenkovski <stamenkovskimar@gmail.com>
2020-03-06 19:22:22 +0100

    minor fixes

---
+++ NSPersist-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  NSPersist-Bridging-Header.h
+//  NSPersist
+//
+//  Created by Martin Stamenkovski on 3/6/20.
+//
+
+#ifndef NSPersist_Bridging_Header_h
+#define NSPersist_Bridging_Header_h
+
+
+#endif /* NSPersist_Bridging_Header_h */
+
+
+@import Foundation;
+
+FOUNDATION_EXPORT double NSPersistVersionNumber;
+FOUNDATION_EXPORT const unsigned char NSPersistVersionString[];
